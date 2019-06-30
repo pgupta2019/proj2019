a4a8e9e925d991e19a69eb39acc69837dd0bd07a
preeti <ritu.agrawal1110@gmail.com>
2019-06-30 08:44:45 +0530

    Repackaging files

---
+++ stockmarket/src/main/java/com/jpm/stockmarket/model/Stock.java
@@ -0,0 +1,47 @@
+package com.jpm.stockmarket.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * @author preetigupta
+ */
+
+public enum Stock {
+
+    TEA("TEA", StockTypes.COMMON, BigDecimal.valueOf(0), BigDecimal.valueOf(0), BigDecimal.valueOf(100)),
+    POP("POP", StockTypes.COMMON, BigDecimal.valueOf(8), BigDecimal.valueOf(0), BigDecimal.valueOf(100)),
+    ALE("ALE", StockTypes.COMMON, BigDecimal.valueOf(23), BigDecimal.valueOf(0), BigDecimal.valueOf(60)),
+    GIN("GIN", StockTypes.PREFERRED, BigDecimal.valueOf(8), BigDecimal.valueOf(0.02), BigDecimal.valueOf(100)),
+    JOE("JOE", StockTypes.COMMON, BigDecimal.valueOf(13), BigDecimal.valueOf(0), BigDecimal.valueOf(250));
+
+
+    @Getter @Setter
+    private String symbol;
+
+    @Getter @Setter
+    private StockTypes type;
+
+    @Getter @Setter
+    private BigDecimal lastDividend;
+    @Getter @Setter
+    private BigDecimal fixedDividend;
+    @Getter @Setter
+    private BigDecimal parValue;
+
+    Stock(String symbol, StockTypes type, BigDecimal lastDividend, BigDecimal fixedDividend, BigDecimal parValue) {
+        this.symbol = symbol;
+        this.type = type;
+        this.lastDividend = lastDividend;
+        this.fixedDividend = fixedDividend;
+        this.parValue = parValue;
+
+    }
+
+    public enum StockTypes {
+
+        COMMON, PREFERRED, OTHER
+    }
+}
